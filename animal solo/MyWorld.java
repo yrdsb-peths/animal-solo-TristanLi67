34027e805241e573b07b8fbd81c91116c45fb783
TristanLi67 <348813510@gapps.yrdsb.ca>
2025-11-30 19:55:11 -0500

    pt 3
    added title screen and made it challenging over time

--- animal solo/MyWorld.java
+++ animal solo/MyWorld.java
@@ -10,6 +10,7 @@ public class MyWorld extends World
 {
     public int score = 0;
     Label scoreLabel;
+    int level = 1;
     /**
      * Constructor for objects of class MyWorld.
      * 
@@ -31,6 +32,7 @@ public class MyWorld extends World
     public void createApple()
     {
         Apple apple = new Apple();
+        apple.setSpeed(level);
         int x = Greenfoot.getRandomNumber(600);
         int y = 0;
         addObject(apple, x, y);
@@ -46,5 +48,10 @@ public class MyWorld extends World
     {
         score++;
         scoreLabel.setValue(score);
+        
+        if(score % 5 == 0)
+        {
+            level += 1;
+        }
     }
 }
